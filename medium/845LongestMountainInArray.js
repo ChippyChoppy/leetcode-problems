e68f8f2a08f7b552adde55b98bf69349c0acb71f
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-08-19 01:53:18 -0400

    longest mountain

---
+++ medium/845LongestMountainInArray.js
@@ -0,0 +1,67 @@
+/**
+ * You may recall that an array arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some index i (0-indexed) with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Given an integer array arr, return the length of the longest subarray, which is a mountain. Return 0 if there is no mountain subarray.
+
+ 
+
+Example 1:
+
+Input: arr = [2,1,4,7,3,2,5]
+Output: 5
+Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+Example 2:
+
+Input: arr = [2,2,2]
+Output: 0
+Explanation: There is no mountain.
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+
+ */
+
+function longestMountain(arr) {
+    let longMount = 0, tempStart = -1; tempEnd = -1;
+
+    if (arr.length < 3) {
+        return 0;
+    }
+
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i] < arr[i + 1] ) {
+            if (tempEnd !== -1) {
+                tempEnd = -1
+                tempStart = -1
+            }
+            if (tempStart === -1) {
+                tempStart = i
+            }
+        } else {
+            if (arr[i] > arr[i + 1]) {
+                if (tempStart !== -1) {
+                    tempEnd = i + 1
+                }
+                if (tempEnd !== -1 && tempStart !== -1) {
+                    if (longMount < tempEnd - tempStart + 1) {
+                        longMount = tempEnd - tempStart + 1
+                    }
+                }
+            } else {
+                tempEnd = -1;
+                tempStart = -1;
+            }
+        }
+    }
+    return longMount
+
+}
+
+console.log(longestMountain([2,1,4,7,3,2,5]))
