5c2ba2e0daa076c5bede9fcf3de22c925d150add
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-08-28 23:22:32 -0400

    almost there

---
+++ medium/36ValidSudoku.js
@@ -0,0 +1,78 @@
+/**
+ * 36. Valid Sudoku --> Medium
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+ */
+
+const isValidSudoku = (board) => {
+    for (let i = 0; i < board.length; j++) {
+        let row = new Set()
+        let column = new Set()
+        let miniBoard = new Set()
+
+        for (let j = 0; j < board.length; j++) {
+            let rNum = board[i][j];
+            let cNum = board[j][i];
+            let miniNum = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
+            if (rNum != '.') {
+                if (row.has(rNum)) return false
+                else row.add(rNum)
+            }
+            if (cNum != '.') {
+                if (column.has(cNum)) return false 
+                else column.add(cNum)
+            }
+            if (miniNum != '.') {
+                if (miniBoard.has(miniNum)) return false
+                else miniBoard.add(miniNum)
+            }
+        }
+    }
+    return true
+}
+
+console.log(isValidSudoku([["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
