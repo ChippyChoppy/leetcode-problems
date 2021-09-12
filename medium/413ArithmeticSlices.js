567f6de87ee592fd8ac6500116a12ad9ba9bd8e4
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-09-11 23:53:13 -0400

    slicety slice

--- medium/413ArithmeticSlices.js
+++ medium/413ArithmeticSlices.js
@@ -3,5 +3,14 @@ const numberOfArithmeticSlices = (nums) => {
     let howMany = 0
     let holding = 0
 
-
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+            holding += 1
+            howMany += holding 
+        } else {
+            holding = 0
+        }
+    }
+    return howMany;
 }
+console.log(numberOfArithmeticSlices([1,2,3,5,6,7,8,10,12,14]))
