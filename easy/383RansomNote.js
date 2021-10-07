d25c397dd7c0f5dd56beda4f5c5f82f8464fda46
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-10-07 14:51:00 -0400

    writing ransom notes

---
+++ easy/383RansomNote.js
@@ -0,0 +1,37 @@
+/**
+ * 383. Ransom Note
+Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+Constraints:
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters. 
+*/
+
+const canConstruct = function(ransomeNote, magazine) {
+    const letterBank = new Map()
+    for (let char of magazine) {
+        letterBank[char] ? letterBank[char]++ : letterBank[char] = 1
+        console.log("available", letterBank)
+    }
+    for (let char of ransomeNote) {
+        if (!letterBank[char]) return false;
+        letterBank[char]--
+        console.log("leftover", letterBank)
+    }
+    return true 
+}
+
+console.log(canConstruct("give me money","abcdef ghijklmno pqrstuvwxyz eem"))
